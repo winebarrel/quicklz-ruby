35ddc00110c6c43727560ef0787af890a169a3f1
winebarrel <sgwr_dts@yahoo.co.jp>
2011-01-13 00:53:12 +0900

    add proc dowload

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,2 +1,18 @@
 require 'mkmf'
+require 'open-uri'
+
+def download(url)
+  dir = File.expand_path(File.dirname(__FILE__))
+  dst = File.join(dir, File.basename(url))
+
+  open(url) do |src|
+    open(dst, 'w') do |f|
+      f << src.read
+    end
+  end
+end
+
+download 'http://www.quicklz.com/quicklz.h'
+download 'http://www.quicklz.com/quicklz.c'
+
 create_makefile('qlzruby')
